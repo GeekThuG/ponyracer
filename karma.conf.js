1206ee81a7e3d498f9e4e3751a2a5aa8706cc5b8
CORP\hfenyom <herve.toukam12@gmail.com>
2020-11-23 11:10:54 +0100

    add 0. Getting started

--- karma.conf.js
+++ karma.conf.js
@@ -4,28 +4,35 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    browserNoActivityTimeout: 30000,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
+      require('karma-firefox-launcher'),
+      require('karma-json-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/ponyracer'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'json-summary' }, { type: 'text-summary' }]
     },
-    reporters: ['progress', 'kjhtml'],
+    jsonReporter: {
+      stdout: false,
+      outputFile: './results/karma-results.json'
+    },
+    reporters: process.env.CI === 'true' ? ['dots', 'json'] : ['progress', 'json', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: process.env.CI === 'true' ? ['ChromeHeadless'] : ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
